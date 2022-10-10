3a2da6b5ad09f80e3ee4117f71bc64879fb3575b
ShySpaceSheep <82251402+ShySpaceSheep@users.noreply.github.com>
2022-10-10 15:39:38 +0800

    Update instructions-set.h

--- instructions-set.h
+++ instructions-set.h
@@ -1,9 +1,9 @@
 #include <instructions.h>
-int addFunc(int x, int y) {
-    return x + y;
-}
-
 
 // Add the list of instructions using our "Instructions" template.
-const auto ADD = new Instruction("ADD", addFunc(x, y));
-
+const Instruction* NOP = new Instruction("NOP", "00000000");
+const Instruction* ADD = new Instruction("ADD", "00000001");
+const Instruction* SUB = new Instruction("SUB", "00000010");
+const Instruction* MOV = new Instruction("MOV", "00000011");
+const Instruction* INC = new Instruction("INC", "00000100");
+const Instruction* JMP = new Instruction("JMP", "00000101");
